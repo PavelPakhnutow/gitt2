a04ab22d5a1f63e12ea6af5b3e174f8d25f5c164
PavelPakhnutow <pavelpakhnutow@gmail.com>
2019-12-24 03:43:33 +0300

    Methods of *\+-

--- lab/ConsoleApplication1/Class.cpp
+++ lab/ConsoleApplication1/Class.cpp
@@ -2,10 +2,28 @@
 
 float func::sum()
 {
-	res = fir + sec;
+	return res = fir + sec;
 }
 
 float func::sub()
 {
-	res = fir - sec;
+	return res = fir - sec;
+}
+
+float func::mul()
+{
+	return res = fir * sec;
+}
+
+float func::div()
+{
+	return res = fir / sec;
+}
+
+void func::set() //функци€ ввода данных
+{
+	cout << "¬ведите первое число: ";
+	cin >> first;
+	cout << "¬ведите второе число: ";
+	cin >> second;
 }
